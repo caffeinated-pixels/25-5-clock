8a7ee8c27b281740b2a9424df0dfe5b5f204800a
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2022-09-17 14:24:12 -0400

    build: update react to v18

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import './styles/main.scss'
 import '@fontsource/righteous'
 import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const root = createRoot(document.getElementById('root'))
+root.render(<App />)
